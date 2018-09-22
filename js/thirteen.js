1e2a23f16fdbaaa471d7800a517fff665b3fe58e
raihanuddin561 <raihanuddin561@gmail.com>
2018-09-22 09:00:17 +0600

    how to change picture content by clicking on specific pictur by javascript

--- js/thirteen.js
+++ js/thirteen.js
@@ -11,3 +11,33 @@ $("#lname").blur(function(){
 	var fullname = firstname+" "+lastname;
 	$("#tblfullname").text(fullname);
 })
+
+function createContent(){
+	var newHeadingElement = document.createElement("h1");
+	var newParagraphElement = document.createElement("p");
+	newHeadingElement.innerHTML = "This is heading";
+	newParagraphElement.innerHTML = "this is paragraph";
+	document.getElementById("myDiv").appendChild(newHeadingElement);
+	document.getElementById("myDiv").appendChild(newParagraphElement); 
+}
+
+$("#btn").click(function(){
+	createContent();
+});
+
+$("#img1").click(function(){
+	var srcPath = $(this).attr("src");
+	$("#mainImage").attr("src",srcPath);
+});
+$("#img2").click(function(){
+	var srcPath = $(this).attr("src");
+	$("#mainImage").attr("src",srcPath);
+});
+$("#img3").click(function(){
+	var srcPath = $(this).attr("src");
+	$("#mainImage").attr("src",srcPath);
+});
+$("#img4").click(function(){
+	var srcPath = $(this).attr("src");
+	$("#mainImage").attr("src",srcPath);
+});
